cf16e5d5673088e2a8a0d454f878ad1a3b51c0b3
Rico Zhu <33135503+ricozhuthegreat@users.noreply.github.com>
2019-10-14 12:28:53 -0400

    Added Converter.py
     - Created a new Python file that converts any existing .h5 Keras saved model to the TFJS json format with binary weights

---
+++ Converter.py
@@ -0,0 +1,20 @@
+
+import tensorflow as tf
+from tensorflow.python.keras.models import load_model
+
+import tensorflowjs as tfjs
+
+# This function returns a new loaded Keras model from a filename source directory
+def load (filename):
+    return load_model(filename + '.h5')
+
+# The target directory of the converted model
+tfjs_target_dir = './models'
+
+# Load the model from the .h5 file
+model = load('FaceRecognition_CNN')
+
+# Convert the loaded model (FaceRecognition_CNN.h5 in this example) to the Tensorflow.js format
+tfjs.converters.save_keras_model(model, tfjs_target_dir)
+
+print("Conversion Success!")
